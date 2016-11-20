249345e64528d03e40b18d3913979970b642d65a
sky0621 <sky14work@gmail.com>
2016-11-21 02:49:06 +0900

    temp01

--- http/publicresource/main.go
+++ http/publicresource/main.go
@@ -5,5 +5,5 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
-	http.ListenAndServe(":8181", mux)
+	http.ListenAndServe(":80", mux)
 }
