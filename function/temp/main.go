aae3e9f13eecf48e32e8309d2af21a5112d0c412
sky0621 <sky14work@gmail.com>
2017-09-08 02:04:32 +0900

    monad

--- function/temp/main.go
+++ function/temp/main.go
@@ -3,3 +3,42 @@ package main
 func main() {
 }
 
+func mnd(selectDbFunc func(whereStr string) []string) (outFn func(ioTarget []string)) {
+
+	selectResult := selectDbFunc("Taro")
+
+	return func(ioTarget []string) {
+		return selectResult
+	}
+}
+
+type Option struct {
+	element interface{}
+	err     error
+}
+
+func NewOption(element interface{}, err error) Option {
+	return Option{
+		element: element,
+		err:     err,
+	}
+}
+
+func (o Option) Val() interface{} {
+	if o.err != nil {
+		return Unit{}
+	}
+	if o.element == nil {
+		return Unit{}
+	}
+	return o.element
+}
+
+type Unit struct {
+}
+
+type Condition struct {
+	wheres interface{}
+}
+
+type IoMonad func(condition Condition) Option
