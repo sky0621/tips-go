c76316b9eaf97657a4b24e1084e96af547684529
sky0621 <sky14work@gmail.com>
2018-10-24 02:03:16 +0900

    goroutine

---
+++ goroutine/no01/no0102/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go ping()
+	pong()
+}
+
+func ping() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("ping")
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func pong() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("     pong")
+		time.Sleep(100 * time.Millisecond)
+	}
+}
