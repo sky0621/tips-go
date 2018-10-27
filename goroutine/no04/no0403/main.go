41b44cd0a686d1ce1bc05b9eb98dc60cf4b8e0fe
sky0621 <sky14work@gmail.com>
2018-10-28 01:27:40 +0900

    goroutine

---
+++ goroutine/no04/no0403/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+
+	components := createComponents()
+
+	for _, cmp := range components {
+		wg.Add(1)
+		go cmp.exec(wg)
+	}
+
+	wg.Wait()
+}
+
+func createComponents() []component {
+	// けっこうエグい構造を作っておく
+	return []component{
+		newComposite("A", []component{
+			newComposite("A-a", []component{
+				newLeaf("A-a-1"),
+			}),
+			newComposite("A-b", []component{
+				newLeaf("A-b-1"),
+				newLeaf("A-b-2"),
+			}),
+		}),
+		newComposite("B", []component{
+			newLeaf("B-a"),
+			newComposite("B-b", []component{
+				newComposite("B-b-1", []component{
+					newLeaf("B-b-1-1"),
+					newLeaf("B-b-1-2"),
+					newComposite("B-b-1-3", []component{
+						newLeaf("B-b-1-3-1"),
+					}),
+				}),
+			}),
+			newLeaf("B-c"),
+		}),
+		newLeaf("C"),
+		newComposite("D", []component{
+			newComposite("D-d", []component{
+				newLeaf("D-d-1"),
+				newLeaf("D-d-2"),
+				newLeaf("D-d-3"),
+				newLeaf("D-d-4"),
+			}),
+		}),
+	}
+}
+
+// ----------------------------------------------
+// コンポジット構造
+// ----------------------------------------------
+
+type component interface {
+	exec(wg *sync.WaitGroup)
+}
+
+func newComposite(name string, components []component) component {
+	c := &composite{name: name}
+	for _, cmp := range components {
+		c.add(cmp)
+	}
+	return c
+}
+
+type composite struct {
+	name   string
+	childs []component
+}
+
+func (c *composite) exec(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+	fmt.Printf("[COMPOSITE]%s\n", c.name)
+
+	wg2 := &sync.WaitGroup{}
+	for _, child := range c.childs {
+		wg2.Add(1)
+		go child.exec(wg2)
+	}
+	wg2.Wait()
+}
+
+func (c *composite) add(cmp component) {
+	c.childs = append(c.childs, cmp)
+}
+
+func newLeaf(name string) component {
+	return &leaf{name: name}
+}
+
+type leaf struct {
+	name string
+}
+
+func (l *leaf) exec(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+	fmt.Printf("[LEAF]%s\n", l.name)
+}
