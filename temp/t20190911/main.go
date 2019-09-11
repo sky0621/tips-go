3660cf98f5f6420c3b2d5e0bdcd90bd63da6d82e
sky0621 <sky14work@gmail.com>
2019-09-12 08:23:56 +0900

    fizzbuzz, fibo

--- temp/t20190911/main.go
+++ temp/t20190911/main.go
@@ -9,8 +9,20 @@ func main() {
 	for n = 1; n < 30; n++ {
 		fmt.Printf("[%2d] %d\n", n, factorial(n, 1))
 	}
-	var o uint64 = 14197454024290336768
-	fmt.Println(o * 22)
+
+	fmt.Println("")
+	fmt.Println("===================================================================")
+	fmt.Println("")
+
+	for i := 1; i < 100; i++ {
+		fizzbuzz(i)
+	}
+
+	fmt.Println("")
+	fmt.Println("===================================================================")
+	fmt.Println("")
+
+	fmt.Println(fibo(20))
 }
 
 func factorial(n, sum uint64) uint64 {
@@ -18,4 +30,24 @@ func factorial(n, sum uint64) uint64 {
 		return sum
 	}
 	return factorial(n-1, n*sum)
+}
+
+func fizzbuzz(n int) {
+	switch {
+	case n%15 == 0:
+		fmt.Println("fizzbuzz")
+	case n%3 == 0:
+		fmt.Println("fizz")
+	case n%5 == 0:
+		fmt.Println("buzz")
+	default:
+		fmt.Println(n)
+	}
+}
+
+func fibo(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fibo(n-2) + fibo(n-1)
 }
