00cc8e98e1f02dcd88275be8389408c359ad51d8
sky0621 <sky14work@gmail.com>
2019-11-13 21:03:03 +0900

    continue

---
+++ tools/mockery/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sky0621/tips-go/tools/mockery/mocks"
+)
+
+func main() {
+	fmt.Println("mockery")
+	t:=&mocks.ProgrammingLanguage{}
+	fmt.Println(t.GetName())
+}
---
+++ tools/mockery/go.mod
@@ -0,0 +1,3 @@
+module github.com/sky0621/tips-go/tools/mockery
+
+require github.com/stretchr/testify v1.4.0
