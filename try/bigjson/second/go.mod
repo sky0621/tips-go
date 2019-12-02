0d022cf3fb60d3dfe32bf02822a61c1ed52ce58b
sky0621 <sky14work@gmail.com>
2019-12-02 09:27:10 +0900

    second

--- try/bigjson/second/go.mod
+++ try/bigjson/second/go.mod
@@ -1,3 +1,5 @@
 module github.com/sky0621/tips-go/try/bigjson/second
 
 require github.com/pkg/profile v1.4.0
+
+go 1.13
--- try/bigjson/second/main.go
+++ try/bigjson/second/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 	"time"
 
 	"github.com/pkg/profile"
@@ -16,21 +16,63 @@ func main() {
 	defer profile.Start(profile.MemProfile, profile.ProfilePath(".")).Stop()
 	fmt.Println("Start")
 
-	ba, err := ioutil.ReadFile("../big.json")
+	f, err := os.Open("../big.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var bj *BigJSON
-	err = json.Unmarshal(ba, &bj)
+
+	dec := json.NewDecoder(f)
+	t, err := dec.Token()
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("<Open> %T: %v\n", t, t)
+	t, err = dec.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("<Open> %T: %v\n", t, t)
+	t, err = dec.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("<Open> %T: %v\n", t, t)
+	t, err = dec.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("<Open> %T: %v\n", t, t)
+	t, err = dec.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("<Open> %T: %v\n", t, t)
+
+	var items []*Item
+	for dec.More() {
+		var item *Item
+		err := dec.Decode(&item)
+		if err != nil {
+			log.Fatal(err)
+		}
+		items = append(items, item)
+		//s, _ := dec.Token()
+		// do something with s which is the newly read string
+		//fmt.Printf("read %q\n", s)
+	}
+	//t, err = dec.Token()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("<Close> %T: %v\n", t, t)
 
 	fmt.Printf("%f秒\n", time.Now().Sub(st).Seconds())
 	fmt.Println("Unmarshaled")
 
-	//for n, item := range bj.Data.Items {
-	//	fmt.Printf("[%d] %#v\n", n, item)
+	//for n, item := range items {
+	//	if n == 100 {
+	//		fmt.Printf("[%d] %#v\n", n, item)
+	//	}
 	//}
 
 	//fmt.Printf("%f秒\n", time.Now().Sub(st).Seconds())
