2de847894dceb8dec767e947166a29459c630a01
sky0621 <sky14work@gmail.com>
2020-01-12 20:18:18 +0900

    temp

--- try/category/main.go
+++ try/category/main.go
@@ -1,28 +1,79 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println("Hello, playground")
 
+	o1 := NewObject()
+	o2 := NewObject()
+
+	o1.Add(NewArrow(o1, o2))
+
+	c := NewCategory([]Object{o1, o2})
+	fmt.Println(c.IsMonoid())
 }
 
-func NewCategory() Category {
-	return &category{}
+func NewCategory(objects []Object) Category {
+	return &category{
+		objects: objects,
+	}
 }
 
+// 圏
 type Category interface {
+	IsMonoid() bool
 }
 
 type category struct {
 	objects []Object
 }
 
-func NewObject() Object {
-	return &object{}
+// 対象がただ1つの圏をモノイドと言う
+func (c *category) IsMonoid() bool {
+	return c.objects != nil && len(c.objects) == 1
 }
 
+func NewObject() Object {
+	o := &object{}
+	// 恒等射は必ず存在
+	identity := NewArrow(o, o)
+	o.arrows = append(o.arrows, identity)
+	return o
+}
+
+// 対象
 type Object interface {
+	Identity() Arrow
+	Add(a Arrow) bool
+	Adds(as []Arrow) bool
 }
 
 type object struct {
+	arrows []Arrow
+}
+
+// 恒等射
+func (o *object) Identity() Arrow {
+	return o.arrows[0]
+}
+
+func (o *object) Add(a Arrow) bool {
+	if a == nil {
+		return false
+	}
+	o.arrows = append(o.arrows, a)
+	return true
+}
+
+func (o *object) Adds(as []Arrow) bool {
+	if as == nil {
+		return false
+	}
+	o.arrows = append(o.arrows, as...)
+	return true
 }
 
 func NewArrow(domain, codomain Object) Arrow {
@@ -32,26 +83,11 @@ func NewArrow(domain, codomain Object) Arrow {
 	}
 }
 
+// 射
 type Arrow interface {
-	Domain() Object
-	Codomain() Object
 }
 
 type arrow struct {
 	domain   Object
 	codomain Object
-}
-
-func (a *arrow) Domain() Object {
-	if a == nil {
-		return nil
-	}
-	return a.domain
-}
-
-func (a *arrow) Codomain() Object {
-	if a == nil {
-		return nil
-	}
-	return a.codomain
 }
