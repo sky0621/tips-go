858854c427aedd7478c13828c3996b70ec0bfa92
sky0621 <sky14work@gmail.com>
2020-06-14 23:15:20 +0900

    temp

--- tools/sqlboiler/src/cmd/main.go
+++ tools/sqlboiler/src/cmd/main.go
@@ -10,28 +10,32 @@ import (
 
 func main() {
 	/*
-	 *
+	 * DB接続準備
 	 */
-	dataSourceName := "dbname=boilerdb user=postgres password=localpass sslmode=false port=21340"
-	db, err := sql.Open("postgres", dataSourceName)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if db != nil {
-			if err := db.Close(); err != nil {
-				panic(err)
-			}
+	var db *sql.DB
+	{
+		dataSourceName := "dbname=boilerdb user=postgres password=localpass sslmode=false port=21340"
+		var err error
+		db, err = sql.Open("postgres", dataSourceName)
+		if err != nil {
+			panic(err)
 		}
-	}()
+		defer func() {
+			if db != nil {
+				if err := db.Close(); err != nil {
+					panic(err)
+				}
+			}
+		}()
 
-	boil.DebugMode = true
+		boil.DebugMode = true
 
-	var loc *time.Location
-	loc, err = time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-		panic(err)
+		var loc *time.Location
+		loc, err = time.LoadLocation("Asia/Tokyo")
+		if err != nil {
+			panic(err)
+		}
+		boil.SetLocation(loc)
 	}
-	boil.SetLocation(loc)
 
 }
