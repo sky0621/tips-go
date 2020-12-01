e48fed3599f015c80ef2c43594764e6a96aaa195
sky0621 <sky14work@gmail.com>
2020-12-02 00:21:58 +0900

    dbconn

---
+++ try/dbconn/src/pg01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// MEMO: ローカルでしか使わないので、ベタ書き
+	dsn := "host=localhost port=23456 dbname=tips-go-try-dbconn-db user=postgres password=yuckyjuice sslmode=disable"
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if db != nil {
+			// It is rare to Close a DB, as the DB handle is meant to be long-lived and shared between many goroutines.
+			if err := db.Close(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+	log.Println("Yes")
+
+	r := chi.NewRouter()
+	r.HandleFunc("/customers", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Queryx("SELECT * FROM customer")
+		if err != nil {
+			if _, err := w.Write([]byte(err.Error())); err != nil {
+				log.Fatal(err)
+			}
+		}
+		if rows == nil {
+			log.Fatal("rows == nil")
+		}
+		for rows.Next() {
+			var c Customer
+			if err := rows.StructScan(&c); err != nil {
+				log.Fatal(err)
+			}
+			if _, err := w.Write([]byte(fmt.Sprintf("%#+v", c))); err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		panic(err)
+	}
+}
+
+type Customer struct {
+	ID   int64
+	Name string
+	Age  int64
+}
---
+++ try/dbconn/src/pg02/go.mod
@@ -0,0 +1,3 @@
+module github.com/sky0621/tips-go/try/dbconn/src/pg02
+
+go 1.15
