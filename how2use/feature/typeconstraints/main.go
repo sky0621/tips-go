12bf4892304e73892b5f7e674568fb302795f94b
sky0621 <sky14work@gmail.com>
2024-12-30 23:30:22 +0900

    -

--- how2use/feature/typeconstraints/main.go
+++ how2use/feature/typeconstraints/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	printInt(3)
 
 	mi := MyInt(8)
 	printInt(mi)
+
+	mt := MyTypes[int]{3, 5, 7}
+	mt.doSomething()
 }
 
 func printInt[T ~int](x T) {
@@ -14,3 +19,9 @@ func printInt[T ~int](x T) {
 }
 
 type MyInt int
+
+type MyTypes[T any] []T
+
+func (m MyTypes[T]) doSomething() {
+	fmt.Println(m)
+}
