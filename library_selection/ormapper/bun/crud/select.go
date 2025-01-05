2139e1ca25c1e436b09c8fa739db7e8a5b0a85fe
sky0621 <sky14work@gmail.com>
2025-01-05 18:10:02 +0900

    -

--- library_selection/ormapper/bun/crud/select.go
+++ library_selection/ormapper/bun/crud/select.go
@@ -2,10 +2,20 @@ package crud
 
 import (
 	"context"
+	"fmt"
+	"log"
+
+	"github.com/sky0621/tips-go/library_selection/ormapper/bun/models"
 
 	"github.com/uptrace/bun"
 )
 
 func Select(ctx context.Context, db *bun.DB) {
-	//
+	var allUsers []models.User
+	if err := db.NewSelect().Model(&allUsers).Scan(ctx); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("-- 単一テーブル(users)の全件取得 --")
+	fmt.Println(allUsers)
+
 }
