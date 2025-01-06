aa7ce5026c5eae61c5d8a914c435343dffa24dc0
sky0621 <sky14work@gmail.com>
2025-01-06 22:48:37 +0900

    -

--- library_selection/ormapper/sqlc/crud/select.go
+++ library_selection/ormapper/sqlc/crud/select.go
@@ -2,8 +2,42 @@ package crud
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/sky0621/tips-go/library_selection/ormapper/sqlc/infra"
 )
 
-func Select(ctx context.Context, q *infra.Queries) {}
+func Select(ctx context.Context, q *infra.Queries) {
+	user, err := q.GetUser(ctx, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("-- 単一テーブル(users)の１件取得 --")
+	fmt.Println(user)
+
+	allUsers, err := q.ListUsers(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("-- 単一テーブル(users)の全件取得 --")
+	fmt.Println(allUsers)
+
+	postsByUser, err := q.ListPostsByUser(ctx, sql.NullInt64{
+		Int64: 1,
+		Valid: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("-- userに紐づくposts取得 --")
+	fmt.Println(postsByUser)
+
+	userWithPostAndComments, err := q.ListUserWithPostAndComments(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("-- 紐づくpostsとcommentsも含めてusers取得 --")
+	fmt.Println(userWithPostAndComments)
+}
---
+++ library_selection/ormapper/sqlc/infra/model_strings.go
@@ -0,0 +1,63 @@
+package infra
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (u User) String() string {
+	var builder strings.Builder
+	builder.WriteString("{")
+	builder.WriteString(fmt.Sprintf("ID: %d, ", u.ID))
+	builder.WriteString(fmt.Sprintf("Name: %s, ", u.Name))
+	builder.WriteString(fmt.Sprintf("CreatedAt: %s, ", u.CreatedAt))
+	builder.WriteString(fmt.Sprintf("UpdatedAt: %s", u.UpdatedAt))
+	builder.WriteString("}")
+	return builder.String()
+}
+
+func (p Post) String() string {
+	var builder strings.Builder
+	builder.WriteString("{")
+	builder.WriteString(fmt.Sprintf("ID: %d, ", p.ID))
+	builder.WriteString(fmt.Sprintf("Title: %s, ", p.Title))
+	builder.WriteString(fmt.Sprintf("Content: %s, ", p.Content))
+	builder.WriteString(fmt.Sprintf("UserID: %d, ", p.UserID))
+	builder.WriteString(fmt.Sprintf("CreatedAt: %v, ", p.CreatedAt))
+	builder.WriteString(fmt.Sprintf("UpdatedAt: %v", p.UpdatedAt))
+	builder.WriteString("}")
+	return builder.String()
+}
+
+func (c Comment) String() string {
+	var builder strings.Builder
+	builder.WriteString("{")
+	builder.WriteString(fmt.Sprintf("ID: %d, ", c.ID))
+	builder.WriteString(fmt.Sprintf("Content: %s, ", c.Content))
+	builder.WriteString(fmt.Sprintf("UserID: %d, ", c.UserID))
+	builder.WriteString(fmt.Sprintf("PostID: %d, ", c.PostID))
+	builder.WriteString(fmt.Sprintf("CreatedAt: %v, ", c.CreatedAt))
+	builder.WriteString(fmt.Sprintf("UpdatedAt: %v", c.UpdatedAt))
+	builder.WriteString("}")
+	return builder.String()
+}
+
+func (u ListUserWithPostAndCommentsRow) String() string {
+	var builder strings.Builder
+	builder.WriteString("{")
+	builder.WriteString(fmt.Sprintf("UserID: %d, ", u.UserID))
+	builder.WriteString(fmt.Sprintf("UserName: %s, ", u.UserName))
+	builder.WriteString(fmt.Sprintf("UserCreatedAt: %v, ", u.UserCreatedAt))
+	builder.WriteString(fmt.Sprintf("UserUpdatedAt: %v, ", u.UserUpdatedAt))
+	builder.WriteString(fmt.Sprintf("PostID: %d, ", u.PostID))
+	builder.WriteString(fmt.Sprintf("PostTitle: %s, ", u.PostTitle))
+	builder.WriteString(fmt.Sprintf("PostContent: %v, ", u.PostContent))
+	builder.WriteString(fmt.Sprintf("PostCreatedAt: %v, ", u.PostCreatedAt))
+	builder.WriteString(fmt.Sprintf("PostUpdatedAt: %v, ", u.PostUpdatedAt))
+	builder.WriteString(fmt.Sprintf("CommentID: %d, ", u.CommentID))
+	builder.WriteString(fmt.Sprintf("CommentContent: %v, ", u.CommentContent))
+	builder.WriteString(fmt.Sprintf("CommentCreatedAt: %v, ", u.CommentCreatedAt))
+	builder.WriteString(fmt.Sprintf("CommentUpdatedAt: %v", u.CommentUpdatedAt))
+	builder.WriteString("}")
+	return builder.String()
+}
