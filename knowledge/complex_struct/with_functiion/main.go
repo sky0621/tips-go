884b03ab63dd09198a92964bb879e3a81ba7e594
sky0621 <sky14work@gmail.com>
2025-02-08 22:22:12 +0900

    -

--- knowledge/complex_struct/with_functiion/main.go
+++ knowledge/complex_struct/with_functiion/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type PlayFunc func(what string) error
+
+type Player struct {
+	Name string
+	Play PlayFunc
+}
+
+func (p Player) execPlay(what string) error {
+	return p.Play(what)
+}
+
+func main() {
+	p1 := Player{
+		Name: "Taro",
+		Play: func(what string) error {
+			switch what {
+			case "カードゲーム":
+				fmt.Println("それで遊ぼう！")
+				return nil
+			case "ボードゲーム":
+				fmt.Println("まあ、それでもいいか。")
+				return nil
+			default:
+				return fmt.Errorf("%sでは遊べない。", what)
+			}
+		},
+	}
+	if err := p1.execPlay("カードゲーム"); err != nil {
+		fmt.Println(err)
+	}
+	if err := p1.execPlay("ボードゲーム"); err != nil {
+		fmt.Println(err)
+	}
+	if err := p1.execPlay("スポーツ"); err != nil {
+		fmt.Println(err)
+	}
 }
