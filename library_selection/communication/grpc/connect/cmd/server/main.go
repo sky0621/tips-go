26ac6162f01907028cdecb9133f772204e1fd572
sky0621 <sky14work@gmail.com>
2025-02-18 20:44:44 +0900

    -

--- library_selection/communication/grpc/connect/cmd/server/main.go
+++ library_selection/communication/grpc/connect/cmd/server/main.go
@@ -9,8 +9,11 @@ import (
 	"github.com/sky0621/tips-go/library_selection/communication/grpc/connect/gen/todo/v1/todov1connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/protobuf/types/known/durationpb"
 	"log"
 	"net/http"
+	"time"
 )
 
 type TodoServer struct {
@@ -62,10 +65,8 @@ func todoByID(id int32) (*todov1.Todo, error) {
 	case id == 400:
 		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid id"))
 	case id == 1400:
-		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid id"))
-	case id == 2400:
 		return nil, connect.NewError(connect.CodeFailedPrecondition, errors.New("invalid id"))
-	case id == 3400:
+	case id == 2400:
 		return nil, connect.NewError(connect.CodeOutOfRange, errors.New("invalid id"))
 	case id == 401:
 		return nil, connect.NewError(connect.CodeUnauthenticated, errors.New("unauthenticated"))
@@ -88,11 +89,17 @@ func todoByID(id int32) (*todov1.Todo, error) {
 	case id == 2500:
 		return nil, connect.NewError(connect.CodeDataLoss, errors.New("data loss"))
 	case id == 501:
-		return nil, connect.NewError(connect.CodeUnimplemented, errors.New("Unimplemented"))
+		return nil, connect.NewError(connect.CodeUnimplemented, errors.New("unimplemented"))
 	case id == 503:
-		return nil, connect.NewError(connect.CodeUnavailable, errors.New("Unavailable"))
+		return nil, connect.NewError(connect.CodeUnavailable, errors.New("unavailable"))
 	case id == 504:
-		return nil, connect.NewError(connect.CodeDeadlineExceeded, errors.New("deadline exceeded"))
+		ce := connect.NewError(connect.CodeDeadlineExceeded, errors.New("deadline exceeded"))
+		retryInfo := &errdetails.RetryInfo{RetryDelay: durationpb.New(1 * time.Second)}
+		time.Sleep(2 * time.Second)
+		if detail, detailErr := connect.NewErrorDetail(retryInfo); detailErr != nil {
+			ce.AddDetail(detail)
+		}
+		return nil, ce
 	default:
 		return nil, errors.New("unexpected id")
 	}
--- library_selection/communication/grpc/connect/go.mod
+++ library_selection/communication/grpc/connect/go.mod
@@ -5,6 +5,7 @@ go 1.23.4
 require (
 	connectrpc.com/connect v1.18.1
 	golang.org/x/net v0.35.0
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250212204824-5a70512c5d8b
 	google.golang.org/protobuf v1.36.5
 )
 
